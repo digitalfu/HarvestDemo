16cebefd6b70a74257158f132eb47bb1b7ccb77e
Hamish Coates <h.coates.developer@gmail.com>
2018-10-10 13:43:42 +0100

    Harvest controller now handles invalid argument errors

--- src/Controller/HarvestController.php
+++ src/Controller/HarvestController.php
@@ -5,23 +5,29 @@ namespace App\Controller;
 
 use App\Harvest\HarvestFactory;
 use App\Harvest\HarvestService;
+use InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class HarvestController extends AbstractController
 {
-
-
     public function index(Request $request, HarvestFactory $factory, HarvestService $service){
 
         $date = $request->query->get('date', false);
-        $daysTillHarvest = null;
+
         try {
+
             $harvest = $factory->createHarvestFromDate($date);
             $daysTillHarvest = $service->getDaysTillHarvest($harvest);
-        } catch (\Exception $e) {
-            // do something
+
+        } catch(InvalidArgumentException $ie) {
+
+            // @todo 'Whoops' leaks service details, fix before production usage.
+
+            throw new HttpException(Response::HTTP_BAD_REQUEST, $ie->getMessage());
         }
 
         return new JsonResponse(['days_till_harvest' => $daysTillHarvest]);
